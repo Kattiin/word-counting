8812f6783f04814e84ac54425d1140ac72da4b1a
Elisia <elisia.ivarsson@gmail.com>
2026-09-25 14:34:43 +0200

    tokenize lower case

--- wordfreq.py
+++ wordfreq.py
@@ -1,27 +1,34 @@
 def tokenize(filename):
-    tokens = []
+    tokens = []  # Lista som lagrar alla tokens
 
-    with open(filename, "r") as file: # Öppna filen för läsning 
+    with open(filename, "r") as file:  # Öppna filen för läsning
 
-        for line in file: # Loopa igenom varje rad i filen
-            currentword = "" # Skapa en variabel för det nuvarande ordet
+        for line in file:  # Loopa igenom varje rad i filen
 
-            for char in line: # Loopar igenom varje tecken i ordet
-                if char.isalpha() or char.isdigit(): # Om tecknet är en bokstav eller siffra, lägg till karaktären i currentword
-                    currentword += char
+            current_word = ""  # Skapa en variabel för ordet som byggs upp
+
+            for char in line:  # Loopa igenom varje tecken i raden
+
+                if char.isalpha() or char.isdigit():
+                    # Om tecknet är en bokstav eller siffra läggs det till i current_word
+                    current_word += char.lower()
 
                 else:
-                    if currentword != "": # Om nuvarande tecknet 
-                        tokens.append(currentword)
-                        currentword = ""
+                    if current_word != "":
+                        # Om ett ord har byggts upp läggs det till i tokens
+                        tokens.append(current_word)
+
+                        current_word = ""  # Nollställer current_word
 
                     if not char.isspace():
+                        # Om tecknet inte är ett mellanslag läggs det till som en egen token
                         tokens.append(char)
 
-            if currentword != "":
-                tokens.append(currentword)
+            if current_word != "":
+                # Lägg till sista ordet på raden om det fortfarande finns ett ord kvar
+                tokens.append(current_word)
 
-    return tokens
+    return tokens  # Returnera listan med alla tokens
 
 
 print(tokenize("examples/article1.txt"))
